ecb525d3a8ef6a4e1b7d7dc0edc2da1eb4735750
andreylavrusenko <m>
2023-03-05 19:39:19 +0300

    return config

---
+++ config/db.ts
@@ -0,0 +1,19 @@
+import mysql from "mysql2";
+
+export const union_pool = mysql.createPool({
+    host: process.env.UNION_HOST,
+    user: process.env.UNION_USER,
+    database: process.env.UNION_DB,
+    password: process.env.UNION_PASSWORD,
+    port: 3306
+});
+
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+    port: 3306
+})
