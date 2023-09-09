91431ec0cc7cd499827e4862f02c2650422e2795
andreylavrusenko <m>
2023-09-09 11:49:10 +0300

    image slider

--- components/product/card-slider/CardSlider.tsx
+++ components/product/card-slider/CardSlider.tsx
@@ -1,23 +1,25 @@
-import React, {useState} from 'react';
-import styles from './slider.module.scss'
+import React, {useState} from "react";
+import styles from "./slider.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 interface IProps {
-    image: string
-    background: string
-    images: string[]
-    title: string
+    image: string;
+    background: string;
+    images: string[];
+    title: string;
 }
 
 const CardSlider = ({image, images, background, title}: IProps) => {
-    const [activeImg, setActiveImg] = useState([])
+    const [activeImg, setActiveImg] = useState([]);
+    const [isOpenGallery, setIsOpenGallery] = useState(false);
 
     // @ts-ignore
-    const images_arr = JSON.parse(images)
+    const images_arr = JSON.parse(images);
 
     const getNewImg = (e: any) => {
-        setActiveImg(e.target.src)
-    }
+        setActiveImg(e.target.src);
+    };
 
 
     return (
@@ -30,16 +32,33 @@ const CardSlider = ({image, images, background, title}: IProps) => {
             {/*Картинки из массива*/}
             <div className={styles.cardslider__down}>
                 <div className={styles.cardslider__down__item}>
-                    <Image layout='fill' objectFit='contain' onClick={getNewImg} className={styles.cardslider__down__img} src={process.env.NEXT_S3_LINK + image} alt={title} />
+                    <Image layout="fill" objectFit="contain" onClick={getNewImg} className={styles.cardslider__down__img} src={process.env.NEXT_S3_LINK + image} alt={title} />
                 </div>
 
-                {images_arr.map((item: string, i: number) => (
-                    <div key={i} className={styles.cardslider__down__item}>
-                        <Image layout='fill' objectFit='contain' onClick={getNewImg} className={styles.cardslider__down__img} src={process.env.NEXT_S3_LINK + item.trim()} alt=""/>
-                    </div>
-                ))}
+
+                {
+                    isOpenGallery
+                        ? images_arr.map((item: string, i: number) => (
+                                <div key={i} className={styles.cardslider__down__item}>
+                                    <Image layout="fill" objectFit="contain" onClick={getNewImg} className={styles.cardslider__down__img} src={process.env.NEXT_S3_LINK + item.trim()} alt=""/>
+                                </div>
+                            ))
+                        : images_arr.slice(0,5).map((item: string, i: number) => (
+                            <div key={i} className={styles.cardslider__down__item}>
+                                <Image layout="fill" objectFit="contain" onClick={getNewImg} className={styles.cardslider__down__img} src={process.env.NEXT_S3_LINK + item.trim()} alt=""/>
+                            </div>
+                        ))
+                }
 
             </div>
+
+            {images_arr.length > 6
+                ? <div className={styles.cardslider__show_more} onClick={() => setIsOpenGallery(!isOpenGallery)}>{isOpenGallery ? 'Скрыть': 'Показать еще'}</div>
+                : null
+            }
+
+
+
         </div>
     );
 };
